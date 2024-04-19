a7ab8aa85ef736a3ec3a19743efc02afd44ff5a7
nishanthan <nishanthan@codevus.com>
2024-04-19 14:08:03 +0530

    feature: creating a demo post

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 var WPAPI = require( 'wpapi' );
-var wp = new WPAPI({ endpoint: 'http://src.wordpress-develop.dev/wp-json' });
+console.log('Starting...');
 
+// You must authenticate to be able to POST (create) a post
+var wp = new WPAPI({
+    endpoint: 'https://nishanthan47.wordpress.com/wp-json/',
+    username: 'nishanthan@enlear.com',
+    password: 'IgE#7jTy^Goax#%8o0Dy0CgH'
+});
+
+//creating a demo post
+wp.posts().create({
+    title: 'Title',
+    content: 'Example content',
+    status: 'publish'
+}).then(function( response ) {
+    // "response" will hold all properties of your newly-created post,
+    console.log( response.id );
+})
