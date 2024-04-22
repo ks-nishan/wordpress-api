d06bd7286d269b5026d2c8ebda71976b0fbc0a0d
nishanthan <nishanthan@codevus.com>
2024-04-22 10:25:19 +0530

    updated pw

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ console.log('Starting...');
 
 // You must authenticate to be able to POST (create) a post
 var wp = new WPAPI({
-    endpoint: 'https://nishanthan47.wordpress.com/wp-json/',
+    endpoint: 'https://nishanthan47.wordpress.com/wp-json/wp/v2/',
     username: 'nishanthan@enlear.com',
-    password: 'IgE#7jTy^Goax#%8o0Dy0CgH'
+    password: '2ayacwxfstlew6nq'
 });
 
 // Function to create a demo post
 async function createDemoPost() {
     try {
+        console.log('post is loading...')
         const response = await wp.posts().create({
             title: 'Title',
             content: 'Example content',
